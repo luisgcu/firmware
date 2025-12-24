19e91feb2224121abaa749e28a1a3cff09f6168e
luisgcu <angelgcu@gmail.com>
2025-12-24 12:56:11 -0500

    Update platformio.ini and variant.h for esp32c3_super_mini configuration adjustments

--- variants/esp32c3/diy/esp32c3_super_mini/variant.h
+++ variants/esp32c3/diy/esp32c3_super_mini/variant.h
@@ -23,7 +23,12 @@ extern "C" {
 #define GPS_TX_PIN (21)
 
 // Button
-#define BUTTON_PIN (9) // BOOT button
+//#define BUTTON_PIN (9) // BOOT button
+// Battery voltage measurement
+#define BATTERY_PIN (2)  // ADC1_CH2
+#define ADC_CHANNEL ADC1_CHANNEL_2  // ESP32-C3 ADC channel for GPIO2
+#define ADC_MULTIPLIER 2.025 // Adjust based on your voltage divider ( was 2.004 MC31)
+#define ADC_ATTENUATION ADC_ATTEN_DB_12 // 0-3.3V range (replaces deprecated ADC_ATTEN_DB_11)
 
 // LoRa
 #define USE_LLCC68
@@ -31,15 +36,16 @@ extern "C" {
 // #define USE_RF95
 #define USE_SX1268
 
+/// Set pinout for  XIAO ESP32C3+ SX1262
 #define LORA_DIO0 RADIOLIB_NC
-#define LORA_RESET (5)
-#define LORA_DIO1 (3)
-#define LORA_RXEN (2)
-#define LORA_BUSY (4)
-#define LORA_SCK (10)
-#define LORA_MISO (6)
-#define LORA_MOSI (7)
-#define LORA_CS (8)
+#define LORA_RESET (4)     //was 5
+#define LORA_DIO1 (3)      //was 3
+#define LORA_RXEN (7)      //was 2
+#define LORA_BUSY (5)      //was 4 
+#define LORA_SCK (8)      //was 10
+#define LORA_MISO (9)      //was 6
+#define LORA_MOSI (10)      //was 7
+#define LORA_CS (6)        //was 8
 
 #define SX126X_CS LORA_CS
 #define SX126X_DIO1 LORA_DIO1
